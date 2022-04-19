f2cc0993697763a44d6680672f3693c5ca22bb09
Bryan Costa <bryancmoraes776@gmail.com>
2022-04-18 22:17:55 -0300

    date Output

--- src/index.js
+++ src/index.js
@@ -11,47 +11,51 @@ app.get('/calculator', (req, res) => {
 });
 
 app.post('/addition', (req, res) => {
+    // req.body - variaveis foram modificadas como const
     const { value1, value2 } = req.body;
 
-    const resultValue1 = Number(value1);
-    const resultValue2 = Number(value2);
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+        res.status(400).json({ error: 'Os valores precisam estar no formato Number' });
+    } else {
+        const resultAddition = value1 + value2;
+        res.status(201).json(resultAddition);
+    }
 
-    const resultAddition = resultValue1 + resultValue2;
-
-    res.status(201).json(resultAddition);
+    /*   const resultValue1 = Number(value1);
+    const resultValue2 = Number(value2); */
 });
 
 app.post('/subtraction', (req, res) => {
     const { value1, value2 } = req.body;
 
-    const resultValue1 = Number(value1);
-    const resultValue2 = Number(value2);
-
-    const resultSubtraction = resultValue1 - resultValue2;
-
-    res.status(201).json(resultSubtraction);
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+        res.status(400).json({ error: 'Os valores precisam estar no formato Number' });
+    } else {
+        const resultSubtracion = value1 - value2;
+        res.status(201).json(resultSubtracion);
+    }
 });
 
 app.post('/division', (req, res) => {
     const { value1, value2 } = req.body;
 
-    const resultValue1 = Number(value1);
-    const resultValue2 = Number(value2);
-
-    const resultDivision = resultValue1 / resultValue2;
-
-    res.status(200).json(resultDivision);
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+        res.status(400).json({ error: 'Os valores precisam estar no formato Number' });
+    } else {
+        const resultDivision = value1 / value2;
+        res.status(201).json(resultDivision);
+    }
 });
 
 app.post('/multiplication', (req, res) => {
     const { value1, value2 } = req.body;
 
-    const resultValue1 = Number(value1);
-    const resultValue2 = Number(value2);
-
-    const resultMultiplication = resultValue1 * resultValue2;
-
-    res.status(201).json(resultMultiplication);
+    if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+        res.status(400).json({ error: 'Os valores precisam estar no formato Number' });
+    } else {
+        const resultMultiplication = value1 * value2;
+        res.status(201).json(resultMultiplication);
+    }
 });
 
 const PORT = 3000;
