c1574c9f355d1392bf34335310844d606bf38d29
Bryan Costa <bryancmoraes776@gmail.com>
2022-05-15 22:43:52 -0300

    initialized database

--- models/config/database.js
+++ models/config/database.js
@@ -1,5 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/calculator', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.info('Conectado ao Mongodb'))
-    .catch((err) => console.info(err));
+exports.initDatabase = () => mongoose.connect('mongodb://localhost/calculator', { useNewUrlParser: true, useUnifiedTopology: true });
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const equationRoutes = require('../routes/equation.route');
-require('../models/config/database');
+const db = require('../models/config/database');
 
 const app = express();
 
@@ -10,6 +10,12 @@ app.use(equationRoutes);
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    try {
+        await db.initDatabase().then(() => console.info('connected'));
+    } catch (error) {
+        console.info('connection error');
+        return;
+    }
     console.info(PORT);
 });
